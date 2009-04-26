70a3d8441cd92ed903311a239bf4feb470a75536
bct <bct@nyarlathotep.(none)>
2009-04-26 01:54:49 -0600

    maruku -> atom

--- bin/process-maruku.rb
+++ bin/process-maruku.rb
@@ -2,6 +2,7 @@
 
 require 'rubygems'
 require 'maruku'
+require 'atom/entry'
 
 require 'yaml'
 
@@ -13,6 +14,13 @@ def write_default_config_file
     f.write <<END
 # directory to save output files to
 output directory: ./output/
+
+# system-wide author details - please fill in at least the name.
+# (you can also give per-entry author names with the Author attribute)
+#author:
+#  name: The Author
+#  uri: http://example.org/
+#  email: author@example.org
 END
   end
 end
@@ -25,6 +33,34 @@ def load_config
   YAML.load(File.read(CONF_FILE))
 end
 
+require 'rexml/xpath'
+
+# takes a Maruku document, turns it into an Atom::Entry
+def maruku_to_atom(mrk)
+  e = Atom::Entry.new
+
+  e.tag_with mrk.attributes[:tags]
+  e.title     = mrk.attributes[:title]
+  e.updated   = mrk.attributes[:updated]
+
+  if mrk.attributes[:author]
+    e.authors.new :name => mrk.attributes[:author]
+  end
+
+  e.content = ''
+  e.content.type = 'xhtml'
+
+  tree = mrk.to_html_document_tree
+
+  # skip the <h1/> (it's already in the <atom:title/>)
+  REXML::XPath.each(tree, '//body/*[name() != "h1"]') do |el|
+    # unfortunately I didn't expose an atom-tools API for appending elements...
+    e.content.instance_variable_get('@content') << el
+  end
+
+  e
+end
+
 def write_output
   outdir = Conf['output directory']
 
@@ -33,13 +69,17 @@ def write_output
   end
 
   Dir[ENTRIES_DIR + '/*'].each do |entry|
-    doc = Maruku.new(File.read(entry))
+    mrk = Maruku.new(File.read(entry))
+    mrk.attributes[:updated] = File.mtime(entry)
 
-    outname = (File.basename(entry) + '.html')
-    outpath = File.join Conf['output directory'], outname
+    bname = File.basename(entry)
 
-    File.open(outpath, 'w') do |f|
-      f.write doc.to_html_document
+    File.open(File.join(outdir + bname + '.html'), 'w') do |f|
+      f.write mrk.to_html_document
+    end
+
+    File.open(File.join(outdir + bname + '.atom'), 'w') do |f|
+      f.write maruku_to_atom(mrk).to_s
     end
   end
 end
